a7f62c2df3e371cf0cde5b49f1a9853e6a9a0673
Nathan Albuquerque <nathan.nra@outlook.com>
2024-10-26 19:20:55 -0300

    Aula 0101: Apresentacao do curso

---
+++ Aula 0101/app.js
@@ -0,0 +1,35 @@
+
+// React Completo, oque aprenderei neste curso:
+// Componentes Funcionais, Hooks, Ciclo de Vida, Formulários, Propriedades, Router e Mais
+
+
+// GRADE DE ENSINO:
+// 1. React Completo
+// Ferramentas e JavaScript
+
+// 2. React Para Iniciantes
+// React Básico, Vite, JSX, Eventos, Componentes, Propriedades
+
+// 3. React Hooks
+// useState, useEffect, useRef, useMemo, useContext, Custom Hooks, Regras
+
+// 4. Formulários
+// Input, Select, Radio, Checkbox, useForm, Validação
+
+// 5. CSS
+// CSS Import, CSS Modules, Styled Components, Frameworks, Imagens
+
+// 6. React Router
+// Router, Link, useParams, Nested Routes, Head
+
+// 7. Mais React
+// PropTypes, Lazy e Suspense, Memo, useReducer, Classes
+
+// 8. Projeto Final
+// API, Autenticação JWT, Scroll Infinito, Biblioteca Externa, Build, Loading
+
+
+
+
+// Vai direto ao ponto?
+// Não. O objeto do curso não é ir direto ao ponto, e sim fazer você entender o ponto. Eu rodo o ponto, volto no ponto e demonstro o ponto.
