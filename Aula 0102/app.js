efd7a43a6a2e57d3933600d940bad76575c77de6
Nathan Albuquerque <nathan.nra@outlook.com>
2024-10-27 19:30:25 -0300

    Aula 0102: Ferramentas e aambiente de desenvolvimento

---
+++ Aula 0102/app.js
@@ -0,0 +1,83 @@
+// slide: https://www.origamid.com/slide/react-completo/#/0102-ferramentas/1
+// video: https://www.origamid.com/curso/react-completo/0102-ferramentas
+
+// Ferramentas e preparação de ambiente:
+
+// Instalar o Node.js / NPM
+// https://nodejs.org/en/download/
+
+// Instalar o Git
+// https://git-scm.com/downloads
+
+// Instalar o Visual Studio Code
+// https://code.visualstudio.com/
+
+// React Developer Tools
+// https://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi?hl=en
+
+
+
+
+
+
+
+// Visual Studio Code (Extensões) [Opcional]
+
+// Origamid Next Theme
+// https://marketplace.visualstudio.com/items?itemName=origamid.origamid-next
+
+// Live Server, Color Highlight, Prettier, ESLint, ES7+ React/Redux/React-Native/JS snippets
+// Extensions > Buscar > Instalar
+
+// IBM Plex Mono
+// https://fonts.google.com/specimen/IBM+Plex+Mono
+
+
+// Visual Studio Code (Configuração)
+// CTRL/CMD + SHIFT + P > Open Settings (JSON):
+{
+  "editor.fontFamily": "IBM Plex Mono, Monaco, Courier New, monospace",
+  "editor.fontSize": 16,
+  "editor.lineHeight": 28,
+  "editor.tabSize": 2,
+  "editor.tabCompletion": "on",
+  "editor.wordWrap": "on",
+  "workbench.activityBar.visible": true,
+  "liveServer.settings.donotShowInfoMsg": true,
+  "liveServer.settings.donotVerifyTags": true,
+  "explorer.confirmDragAndDrop": false,
+  "diffEditor.renderSideBySide": true,
+  "editor.formatOnSave": true,
+  "html.autoClosingTags": false,
+  "editor.colorDecorators": false,
+  "editor.autoClosingBrackets": "always",
+  "editor.autoClosingQuotes": "always",
+  "prettier.singleQuote": true,
+  "files.associations": {
+    "*.js": "javascriptreact"
+  },
+  "editor.minimap.renderCharacters": false,
+  "breadcrumbs.enabled": false,
+  "telemetry.telemetryLevel": "off",
+  "color-highlight.markerType": "dot-before",
+  "editor.renderWhitespace": "selection",
+  "workbench.statusBar.visible": true,
+  "prettier.trailingComma": "all",
+  "[javascriptreact]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+  },
+  "[json]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+  },
+  "explorer.sortOrder": "default",
+  "window.title": "${activeEditorMedium}${separator}${rootName}",
+  "window.newWindowDimensions": "inherit",
+  "workbench.colorTheme": "Origamid Next",
+  "workbench.iconTheme": "origamid-next-icons",
+  "html.format.wrapAttributes": "auto",
+  "html.format.wrapLineLength": 0,
+  "[html]": {
+    "editor.defaultFormatter": "vscode.html-language-features"
+  },
+  "editor.bracketPairColorization.enabled": false,
+}
